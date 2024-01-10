0e3d62eaa542933b6e79ffc6427bae4d0b87fd94
CarinWood <carin.wood.85@gmail.com>
2024-01-10 13:50:15 +0100

    five first exercises finished

---
+++ index.js
@@ -0,0 +1,31 @@
+const numbers = [2,6,12,7,22,35];
+
+/* Exercise 1 */
+
+const first = numbers[0]
+console.log(first)
+
+
+/* Exercise 2 */
+
+const last = numbers[numbers.length-1]
+console.log(last)
+
+/* Exercise 3 */
+
+const fourth = numbers[3]
+console.log(fourth)
+
+/* Exercise 4 */
+
+const second = numbers[1]
+const fifth = numbers[4]
+
+const newArray = {second, fifth}
+
+console.log(newArray)
+
+/* Exercise 5 */
+
+const lengthOfArray = numbers.length
+console.log("length of numbers array is: " + lengthOfArray)
